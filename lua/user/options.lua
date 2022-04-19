dfa63a0b3c5ab17e42e3bec1fcb62d63f8f9cea7
Ravisankar Sivasubramaniam <rasivasu@gmail.com>
2022-04-19 14:20:49 +0530

    Increased the operator-pending mode timeout and the guifont.

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -17,7 +17,7 @@ local options = {
   splitright = true,                       -- force all vertical splits to go to the right of current window
   swapfile = false,                        -- creates a swapfile
   termguicolors = true,                    -- set term gui colors (most terminals support this)
-  timeoutlen = 100,                        -- time to wait for a mapped sequence to complete (in milliseconds)
+  timeoutlen = 200,                        -- time to wait for a mapped sequence to complete (in milliseconds)
   undofile = true,                         -- enable persistent undo
   updatetime = 300,                        -- faster completion (4000ms default)
   writebackup = false,                     -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
@@ -32,7 +32,7 @@ local options = {
   wrap = false,                            -- display lines as one long line
   scrolloff = 8,                           -- is one of my fav
   sidescrolloff = 8,
-  guifont = "FiraCode Nerd Font Mono:H18",               -- the font used in graphical neovim applications
+  guifont = "JetBrainsMono Nerd Font Mono:h16",               -- the font used in graphical neovim applications
 }
 
 vim.opt.shortmess:append "c"
@@ -41,6 +41,12 @@ for k, v in pairs(options) do
   vim.opt[k] = v
 end
 
-vim.cmd "set whichwrap+=<,>,[,],h,l"
+vim.cmd [[set whichwrap+=<,>,[,],h,l "]]
 vim.cmd [[set iskeyword+=-]]
 vim.cmd [[set formatoptions-=cro]] -- TODO: this doesn't seem to work
+vim.cmd [[
+  set nocompatible
+  if has("autocmd")
+    filetype plugin indent on
+  endif
+]]
