e137818b37682c88cd575c18b3f2d7b1a67adb79
Ravisankar Sivasubramaniam <rasivasu@gmail.com>
2022-12-23 08:23:46 +0530

    Fixed breaking changes

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -45,7 +45,14 @@ nvim_tree.setup {
   open_on_tab = false,
   hijack_cursor = false,
   update_cwd = true,
-  update_to_buf_dir = {
+  actions = {
+    open_file = {
+      quit_on_open = true,
+      resize_window = true,
+      window_picker = true
+    },
+  },
+  hijack_directories = {
     enable = true,
     auto_open = true,
   },
@@ -81,7 +88,6 @@ nvim_tree.setup {
     height = 30,
     hide_root_folder = false,
     side = "left",
-    auto_resize = true,
     mappings = {
       custom_only = false,
       list = {
@@ -97,15 +103,17 @@ nvim_tree.setup {
     cmd = "trash",
     require_confirm = true,
   },
-  quit_on_open = 0,
   git_hl = 1,
-  disable_window_picker = 0,
-  root_folder_modifier = ":t",
-  show_icons = {
-    git = 1,
-    folders = 1,
-    files = 1,
-    folder_arrows = 1,
-    tree_width = 30,
+  renderer = {
+      root_folder_label = ":t",
+  },
+  icons = {
+    show = {
+      git = 1,
+      folder = 1,
+      file = 1,
+      folder_arrows = 1,
+      tree_width = 30,
+    },
   },
 }
